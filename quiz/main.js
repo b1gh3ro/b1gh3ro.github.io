b51e7829af401565ccec7d23f75f35fb6d1a3976
b1gh3ro <73356711+b1gh3ro@users.noreply.github.com>
2020-10-27 12:24:32 +0300

    updte

--- quiz/main.js
+++ quiz/main.js
@@ -1,39 +1,52 @@
-const questiontag = document.querySelector('.question')
-const nextButton = document.querySelector('.next')
-let mixed, index
-
-window.onload = function(){
-  changequestiontag()
-  setquestion()
+const questionelement = document.querySelector('#question')
+const opt1 = document.querySelector('#opt-1')
+const opt2 = document.querySelector('#opt-2')
+const opt3 = document.querySelector('#opt-3')
+const opt4 = document.querySelector('#opt-4')
+const next = document.querySelector('#next')
+let selectques, index
+window.onload = function load(){
+  setq()
+  selectq()
   index = 0
 }
-nextButton.addEventListener('click', function(){
+next.addEventListener('click', function(){
+  selectq()
   index++
-  setquestion()
-  
 })
-
-
-
-function changequestiontag(content){
-  if(!content) content = mixed[0]
-  questiontag.innerHTML = content.question
-   
-}
-  
-
-const thequestions = [{
-  question: 'is my name saad'
+function setq(cont){
+  if(!cont) cont = selectques[0]
+  questionelement.innerHTML = cont.question
+  opt1.innerHTML = cont.opt1
+  opt2.innerHTML = cont.opt2
+  opt3.innerHTML = cont.opt3
+  opt4.innerHTML = cont.opt4
+   }
+const questions = [{
+  question: 'what is 2+2',
+  opt1: '3',
+  opt2:'4',
+  opt3: '5',
+  opt4:'6',
 },
 {
-  question: 'why are you here'
-}
-,
+  question: 'nihga',
+  opt1: 'no',
+  opt2:'4',
+  opt3: '5',
+  opt4:'6',
+},
 {
-question: 'good choice'
+  question: 'cet',
+  opt1: 'yes',
+  opt2:'4',
+  opt3: '5',
+  opt4:'6',
 }]
-function setquestion(){
-    changequestiontag(mixed[index])
-}
-mixed = thequestions.sort();
 
+//selet q
+function selectq(){
+ setq(selectques[index])
+}
+
+selectques = questions.sort();
