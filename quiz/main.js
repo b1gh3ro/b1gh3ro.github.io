dd71e084b107d6a464946b99868e6b779ff3a449
b1gh3ro <73356711+b1gh3ro@users.noreply.github.com>
2020-11-03 17:49:57 +0300

    sss

--- quiz/main.js
+++ quiz/main.js
@@ -4,6 +4,7 @@ const opt2 = document.querySelector('#opt-2')
 const opt3 = document.querySelector('#opt-3')
 const opt4 = document.querySelector('#opt-4')
 const next = document.querySelector('#next')
+const nextbtn = document.querySelector('#nextbtn')
 let selectques, index
 window.onload = function load(){
   setq()
@@ -14,13 +15,34 @@ next.addEventListener('click', function(){
   selectq()
   index++
 })
+
 function setq(cont){
+questionelement.style.display = 'none'
+opt1.style.display = 'none'
+opt2.style.display = 'none'
+opt3.style.display = 'none'
+opt4.style.display = 'none'
+
+nextbtn.innerHTML = 'start'
+next.style.width = '50%'
+nextbtn.style.background = 'white'
   if(!cont) cont = selectques[0]
+  if(index<= questions.length -1){
+    questionelement.style.display = 'block'
+    opt1.style.display = 'block'
+    opt2.style.display = 'block'
+    opt3.style.display = 'block'
+    opt4.style.display = 'block'
+    nextbtn.innerHTML = 'next'
+
   questionelement.innerHTML = cont.question
   opt1.innerHTML = cont.opt1
   opt2.innerHTML = cont.opt2
   opt3.innerHTML = cont.opt3
   opt4.innerHTML = cont.opt4
+}
+
+
    }
 const questions = [{
   question: 'what is 2+2',
@@ -50,3 +72,4 @@ function selectq(){
 }
 
 selectques = questions.sort();
+
